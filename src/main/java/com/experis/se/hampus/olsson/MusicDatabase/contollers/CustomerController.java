b84c7eb98bac53b91e5b705e58b344ed505e9e87
Christopher Berglund <cberg91@hotmail.com>
2020-10-15 16:00:16 +0200

    features getAllCustomers
    added functionality to get all customers from database

--- src/main/java/com/experis/se/hampus/olsson/MusicDatabase/contollers/CustomerController.java
+++ src/main/java/com/experis/se/hampus/olsson/MusicDatabase/contollers/CustomerController.java
@@ -1,5 +1,7 @@
 package com.experis.se.hampus.olsson.MusicDatabase.contollers;
 
+import java.util.ArrayList;
+
 import com.experis.se.hampus.olsson.MusicDatabase.data_access.CustomerRepository;
 import com.experis.se.hampus.olsson.MusicDatabase.models.Customer;
 
@@ -12,7 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     CustomerRepository customerRepo = new CustomerRepository();
 
+    @RequestMapping(value = "/api/customers", method = RequestMethod.GET)
+    public ArrayList<Customer> getAllCustomers(){
+        return customerRepo.getAllCustomers();
+    }
+
     //Set endpoints
     @RequestMapping(value = "api/customers/{id}", method = RequestMethod.GET)
     public Customer getCustomerFromId(@PathVariable int id) { return customerRepo.getCustomerFromId(id);}
 }
+   
+    
+
--- src/main/java/com/experis/se/hampus/olsson/MusicDatabase/data_access/CustomerRepository.java
+++ src/main/java/com/experis/se/hampus/olsson/MusicDatabase/data_access/CustomerRepository.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import com.experis.se.hampus.olsson.MusicDatabase.models.Customer;
 
@@ -41,5 +42,46 @@ public class CustomerRepository {
         }
         return newCustomer;
     }
+    public ArrayList<Customer> getAllCustomers(){
+        ArrayList<Customer> customers = new ArrayList<>();
+        
+        try {
+			//Open the connection
+			connection = DriverManager.getConnection(url);
+			System.out.println("Connection to Chinook has been established.");
+            PreparedStatement prepState = connection.prepareStatement("SELECT CustomerId, FirstName, LastName, Company, Adress, City, State, Country, PostalCode, PhoneNumber, Fax, Email, SupportRedId FROM Customer");
+            ResultSet set = prepState.executeQuery();
+            while(set.next()){
+            customers.add(new Customer(
+                set.getInt("CustomerId"),
+                set.getString("FirstName"),
+                set.getString("LastName"),
+                set.getString("Company"),
+                set.getString("Adress"),
+                set.getString("City"),
+                set.getString("State"),
+                set.getString("Country"),
+                set.getString("PostalCode"),
+                set.getString("PhoneNumber"),
+                set.getString("Fax"),
+                set.getString("Email"),
+                set.getInt("SupportRedId")
+            ));
+            }
+            System.out.println("Fetched new customers.");
 
+		} catch (Exception e){
+			System.out.println("Failed to connect to Chinook.");
+		}
+		finally {
+			try {
+				//Close the connection
+				connection.close();
+			} catch (Exception e) {
+				System.out.println("Failed to close connection to Chinook.");
+			}
+        }
+        return customers;
 }
+}
+
