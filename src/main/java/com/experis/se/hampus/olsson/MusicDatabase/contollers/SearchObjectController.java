a8e663190e1929990b23120fdf5d3056e1cb6b61
Heso113 <hampe_olsson91@hotmail.com>
2020-10-20 11:30:34 +0200

    feature: added a home webpage with search functionality at http://localhost:8080/home.
    The home page lists five random artists, tracks and genres as music suggestions while laso featuring a search field where the user can search for tracks.
    
    The search page features a back button which take the user back to the Home-page and it also lists the tracks from the search results. If there was no input it will prompt that the search field was empty.

--- src/main/java/com/experis/se/hampus/olsson/MusicDatabase/contollers/SearchObjectController.java
+++ src/main/java/com/experis/se/hampus/olsson/MusicDatabase/contollers/SearchObjectController.java
@@ -6,9 +6,10 @@ import java.util.ArrayList;
 import com.experis.se.hampus.olsson.MusicDatabase.data_access.SearchObjectRepository;
 import com.experis.se.hampus.olsson.MusicDatabase.models.SearchObject;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,23 @@ public class SearchObjectController {
     SearchObjectRepository searchRepo = new SearchObjectRepository();
 
     //Set endpoints
-    @RequestMapping(value = "/api/search/{search}", method = RequestMethod.GET)
-    public ArrayList<SearchObject> getAllTracksFromSearch(@PathVariable String search) {return searchRepo.getAllTracksFromSearch(search);} 
+    @RequestMapping(value = "/api/search/", method = RequestMethod.GET)
+    public ArrayList<SearchObject> getAllTracksFromSearch(@RequestParam("search") String search, Model model) {
+        return searchRepo.getAllTracksFromSearch(search);
+    } 
+
+    @RequestMapping(value = "/api/artists/", method = RequestMethod.GET)
+    public ArrayList<SearchObject> getAllArtists() {
+        return searchRepo.getAllUniqueArtists();
+    }
+
+    @RequestMapping(value = "/api/genres/", method = RequestMethod.GET)
+    public ArrayList<SearchObject> getAllGenres() {
+        return searchRepo.getAllUniqueGenres();
+    }
+
+    @RequestMapping(value = "/api/tracks/", method = RequestMethod.GET)
+    public ArrayList<SearchObject> getAllTracks() {
+        return searchRepo.getAllTracks();
+    }
 }
